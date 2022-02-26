605ab1e05fa74546dd58293c9e449c5995602f99
wonki <wonki95891@gmail.com>
2022-02-26 16:55:19 +0900

    2022-02-26 thymeleaf ,h2 DB 설치 JPA & DB 설정 및 쿼리로그 출력

--- jpashop/src/main/java/jpabook/jpashop/JpashopApplication.java
+++ jpashop/src/main/java/jpabook/jpashop/JpashopApplication.java
@@ -8,12 +8,6 @@ public class JpashopApplication {
 
 	public static void main(String[] args) {
 
-		Hello hello = new Hello();
-		hello.setData("dsdsds");
-		String data = hello.getData();
-		System.out.println("data= " + data);
-
-
 		SpringApplication.run(JpashopApplication.class, args);
 	}
 
